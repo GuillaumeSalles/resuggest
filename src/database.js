93080718349e55fa3bc2924fa09257afdeb53aed
Guillaume Salles <guillaume.salles@me.com>
2017-08-27 22:08:29 -0400

    github pages + cleanup

--- src/database.js
+++ src/database.js
@@ -1,10 +1,3 @@
-import Caml_obj from "bs-platform/lib/js/caml_obj.js";
-import $$String from "bs-platform/lib/js/string.js";
-import Caml_string from "bs-platform/lib/js/caml_string.js";
-import Char from "bs-platform/lib/js/char.js";
-import Pervasives from "bs-platform/lib/js/pervasives.js";
-import Curry from "bs-platform/lib/js/curry.js";
-
 import functions from "./generated/functions";
 import generics from "./generated/genericFunctions";
 
